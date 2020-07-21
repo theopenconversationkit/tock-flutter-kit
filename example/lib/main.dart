3e2ef5bc5d7e02f134575e3d7490befd9c97c435
adrien_body <abody@oui.sncf>
2020-07-21 18:19:57 +0200

    fix display

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -21,38 +21,58 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            body: Container(
-      child: Stack(
-        children: <Widget>[
-          Container(
-            height: ChatTextField.displayHeight(context) - ChatTextField.height,
-            color: Colors.deepOrange,
-            child: ListView.builder(
-              itemBuilder: (_, index) => Bubble(
-                text: "coucou $index",
-                index: index,
-                backgroundBubbleColor: Colors.orangeAccent,
-                borderBubbleColor: Colors.deepOrangeAccent,
-                textBubbleColor: Colors.white,
+      home: TockChat(),
+    );
+  }
+}
+
+class TockChat extends StatefulWidget {
+  @override
+  _TockChatState createState() => _TockChatState();
+}
+
+class _TockChatState extends State<TockChat> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        child: Stack(
+          children: <Widget>[
+            Container(
+              height: MediaQuery.of(context).size.height - ChatTextField.height,
+              color: Colors.deepOrange,
+              child: ListView.builder(
+                itemBuilder: (_, index) => Bubble(
+                  text: "coucou $index",
+                  index: index,
+                  backgroundBubbleColor: Colors.orangeAccent,
+                  borderBubbleColor: Colors.deepOrangeAccent,
+                  textBubbleColor: Colors.white,
+                ),
+                itemCount: 10,
+                scrollDirection: Axis.vertical,
+                cacheExtent: 10,
               ),
-              itemCount: 10,
-              scrollDirection: Axis.vertical,
-              cacheExtent: 10,
             ),
-          ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: ChatTextField(
-              backgroundColor: Colors.orangeAccent,
-              deeperColor: Colors.deepOrangeAccent,
-              placeHolderText: "Ecrivez votre message",
-            ),
-          )
-        ],
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: ChatTextField(
+                backgroundColor: Colors.orangeAccent,
+                deeperColor: Colors.deepOrangeAccent,
+                placeHolderText: "Ecrivez votre message",
+              ),
+            )
+          ],
+        ),
       ),
-    )));
+    );
   }
 }
--- test/tock_flutter_kit_test.dart
+++ test/tock_flutter_kit_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:tock_flutter_kit/tock_flutter_kit.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('tock_flutter_kit');
@@ -15,9 +14,5 @@ void main() {
 
   tearDown(() {
     channel.setMockMethodCallHandler(null);
-  });
-
-  test('getPlatformVersion', () async {
-    expect(await TockFlutterKit.platformVersion, '42');
   });
 }
